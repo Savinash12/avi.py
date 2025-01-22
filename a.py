be2b4574e45cf60b6e3edcb8f29fd97e6b262d4a
Avinash <backon153@gmail.con>
2025-01-22 23:18:36 +0545

    number_guess

--- a.py
+++ a.py
@@ -3,3 +3,8 @@ if 5>2:
 x = "Avinash"
 print(type(x)) 
 print("Wel come to the world of Python")
+import requests
+
+response = requests.get('https://api.github.com')
+print(response.status_code)
+print(response.json())
